b2bfd463b228ac1f5f7eede5c817ed9fe3f2d5fa
Mtheuxa <matheusaraujopaulucci@gmail.com>
2025-10-02 00:52:58 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Bendito seja o desconforto que de faz evoluir! ☠️</h1>
+    return <h1>Bendito seja o desconforto que de faz evoluir!! ☠️</h1>
 }
 
 export default Home;
